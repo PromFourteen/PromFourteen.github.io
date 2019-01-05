c19cc80e2c13aa62b2cd60be5d99fa628aeb6098
Max Liubushkin <41510039+PromFourteen@users.noreply.github.com>
2019-01-05 20:34:31 +0300

    Add files via upload

---
+++ github/index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html ng-app>
+<head>
+	<meta charset="utf-8">
+	<title> /_Maxim Liubushkin_\ </title>
+	<link rel="stylesheet"  href="css/bootstrap.min.css">
+	<link rel="stylesheet"  href="css/main.css">
+	<link rel="stylesheet"  href="css/fonts.css">
+	
+	<!--Font Awesome-->
+	<link rel="stylesheet" href="other/font-awesome/css/fontawesome-all.min.css">
+	<!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">-->
+	<link rel="stylesheet" type="text/css" href="other/owl_carousel/OwlCarousel/dist/assets/owl.carousel.css">
+	<link rel="stylesheet" type="text/css" href="other/owl_carousel/OwlCarousel/dist/assets/owl.theme.default.min.css">
+	<link rel="stylesheet" type="text/css" href="other/owl_carousel/OwlCarousel/docs/assets/css/animate.css">
+</head>
+
+<body>	
+
+	<div class="mobile--unavailable">
+		<div class="messages">
+			<span class="message message_ru">Недоступно для мобильных телефонов.</span>
+			<span class="message message_eng">Unavailable for mobile phones.  </span>
+		</div>
+	</div>
+
+	<div class="main_wrapper">
+
+		<div class="fast_info_wrapper">
+			<div class="fast_info">
+				<div class="name_div">
+					<span class="name">
+						Maxim Liubushkin
+					</span>
+				</div>
+
+				<div class="links_nav">
+					<div class="paragraph">
+						<span class="paragraph_text about">
+							About me
+						</span>
+					</div>
+					<div class="paragraph">
+						<span class="paragraph_text home">
+							Home
+						</span>
+					</div>
+					<div class="paragraph">
+						<span class="paragraph_text projects">
+							Projects
+						</span>
+					</div>
+				</div>
+
+				<div class="icons_links">
+					<div class="icon icon_twitter">
+						<a href="https://twitter.com/Em21PYLw1ZV1AKA" target="_blank"><i class="fab fa-twitter"></i></a>
+					</div>
+					<div class="icon icon_vk">
+						<a href="https://vk.com/prom_serious" target="_blank"><i class="fab fa-vk"></i></a>
+					</div>
+					<div class="icon icon_instagram">
+						<a href="https://www.instagram.com/Prom_14_/" target="_blank"><i class="fab fa-instagram"></i></a>
+					</div>
+					<div class="icon icon_facebook">
+						<a href="https://www.facebook.com/profile.php?id=100026360046688" target="_blank"><i class="fab fa-facebook"></i></a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+		<div class="burger_wrapper">
+				
+			<span>Burger</span>
+
+		</div>
+
+		<div class="slides_wrapper">
+			
+			<div class="slide slide_video first active" id="video">
+				<p id="time">00:00:00</p>
+				<p id="text" class="main_name"><b>Maxim Liubushkin</b></p>
+				<div class="volume_change">
+					<i class="fas fa-volume-up turnOn active" id="vol_on"></i>
+					<i class="fas fa-volume-off turnOff" id="vol_off"></i>
+				</div>
+				<video autoplay loop src="mp4/Your_name.mp4" class="video_main_"></video>
+			</div>
+			
+			<div class="slide slide_about middle hidden" id="about">
+				<div class="content_about container">
+		
+					<div class="header_text">
+						<span class="header">
+							About ME
+						</span>
+					</div>
+					<div class="main_content_text">
+
+						<div class="main_content">
+
+							<span class="descr description_ru">
+								Жизнь - невероятно сложная вещь, в которой много неразгаданных человечеством тайн. Мы не знаем, что нас ждет в будущем, как попасть в прошлое, сможем ли мы жить в космосе.
+							</span>
+
+							<span class="descr description_eng">
+								Life is a really complex thing which has a lot of unsolved mysteries of humanity. We don't know what is waiting for us in future, how to get into the past and can we live in space.
+							</span>
+
+							<span class="descr knowledge">
+								Мои знания/My knowledge: 
+								<span class="knowledge_list">C#, HTML-CSS-JavaScript, Java(Android Studio)</span>
+							</span>
+
+						</div>
+
+					</div>
+
+				</div>
+			</div>
+
+			<div class="slide slide_projects last" id="projects">
+				<div class="content_projects container">
+
+					<div class="header_text">
+						<span class="header">
+							My projects
+						</span>
+					</div>
+					<div class="main_content">
+
+						
+						<div class="projects_wrapper">
+							<div class="projects">
+								<div class="row">
+									<div class="col-md-4">
+
+
+										<div class="project">
+											
+											<div class="overlay"></div>
+											<div class="project_info">
+												<a href="http://mxr.zzz.com.ua"  target="_blank">
+													<span class="project_name">
+														Portfolio
+													</span>
+												</a>
+												<ul class="link">
+													<li>
+														<a href="https://rshruslan.000webhostapp.com/first/"  target="_blank">Portfolio</a>
+													</li>
+												</ul>
+											</div>
+
+											<img src="/img/proj_myPortfolio.jpg">
+										</div>
+
+									</div>
+
+									<div class="col-md-4">
+
+
+										<div class="project">
+											
+											<div class="overlay"></div>
+											<div class="project_info">
+												<a href="https://rshruslan.000webhostapp.com/secong/"  target="_blank">
+													<span class="project_name">
+														Orion
+													</span>
+												</a>
+												<ul class="link">
+													<li>
+														<a href="https://rshruslan.000webhostapp.com/second/"  target="_blank">Orion</a>
+													</li>
+												</ul>
+											</div>
+
+											<img src="/img/proj_orion.jpg">
+										</div>
+
+									</div>
+
+									<div class="col-md-4">
+
+
+										<div class="project">
+
+											<div class="overlay"></div>
+											<div class="project_info">
+												<a href="https://rshruslan.000webhostapp.com/first/"  target="_blank">
+													<span class="project_name">
+														Rapael Lecher
+													</span>
+												</a>
+												<ul class="link">
+													<li>
+														<a href="https://rshruslan.000webhostapp.com/first/"  target="_blank">Rapael Lecher</a>
+													</li>
+												</ul>
+											</div>
+
+											<img src="/img/proj_RapaelLecher.jpg" class="project_bcg_img">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+					</div>
+
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script> 
+	<script type="text/javascript" src="other/owl_carousel/OwlCarousel/docs/assets/vendors/jquery.min.js"></script>
+	<script type="text/javascript" src="other/owl_carousel/OwlCarousel/dist/owl.carousel.js"></script>
+
+	<script type="text/javascript">
+		
+	</script>
+
+</body>
+
+</html>
---
+++ github/js/main.js
@@ -0,0 +1,375 @@
+var out = false;
+var slide_video = $(".slide_video");
+var slide_about = $(".slide_about");
+var slide_projects = $(".slide_projects");
+var video = $(".video_main_");
+
+$(document).ready(function(){
+
+    var player;
+    var lastSlide = slide_video;
+    var order = ['video', 'about', 'projects'];
+
+	$(".burger_wrapper").on("click", function(){
+		if( $(this).hasClass("active") )
+			$(this).removeClass("active");
+		else
+			$(this).addClass("active");
+		slide_video.removeClass("hidden");
+		slide_about.removeClass("hidden");
+		slide_projects.removeClass("hidden");
+
+		out==true?out=false:out=true;
+
+		if( out )
+		{
+			$(".fast_info").addClass("active");
+			slide_video.removeClass("active");
+			slide_about.removeClass("active");
+			slide_projects.removeClass("active");
+		}
+		else
+		{
+			$(".fast_info").removeClass("active");
+			$(lastSlide).addClass("active");
+		}
+	});
+
+	/*Lighting start*/
+	$(".slide_video, .slide_projects, .slide_about").hover(function(){
+		if( !$(this).hasClass("active") )
+			$(this).css({'border': '2px solid white'});
+		if( out )
+		{
+			if( $(this).attr('id') == slide_video.attr('id') )//hover to video slide
+				$(".home").addClass("lighting");
+			if( $(this).attr('id') == slide_about.attr('id') )//hover to aboutME slide
+				$(".about").addClass("lighting");
+			if( $(this).attr('id') == slide_projects.attr('id') )//hover to projects slide
+				$(".projects").addClass("lighting");
+		}
+	});
+
+	$(".slide_video, .slide_projects, .slide_about").mouseleave(function(){
+		if( !$(this).hasClass("active") )
+			$(this).css({'border': 'none'});
+		$(".home").removeClass("lighting");
+		$(".about").removeClass("lighting");
+		$(".projects").removeClass("lighting");
+	});
+	/*Lighting end*/
+
+	/*Turn on/off volume start*/
+	$(".turnOn, .turnOff").on("click", function(){
+		if( $(this).attr('id') == "vol_off" )//mute video
+		{
+			$(".turnOn").addClass('active');
+			$(this).removeClass('active');
+			video.prop("volume", 1);
+		}
+		else//unmute video
+		{
+			$(".turnOn").removeClass('active');
+			$(".turnOff").addClass('active');
+			video.prop("volume", 0);
+		}
+	});
+	/*Turn on/off volume end*/
+
+	$(".slide_video, .slide_projects, .slide_about, .paragraph_text").on("click", function(){
+		if(out == false)
+			return;
+
+		$(".fast_info").removeClass("active");
+		var id;
+		if( $(this).hasClass("paragraph_text") )//clicked to a link_nav
+		{
+			var link_nav = $(this);
+			if( link_nav.hasClass("home") )
+				id = slide_video.attr('id');
+			if( link_nav.hasClass("about") )
+				id = slide_about.attr('id');
+			if( link_nav.hasClass("projects") )
+				id = slide_projects.attr('id');
+
+		}
+		else//clicked to a slide
+			id = $(this).attr('id');
+
+		var lastSlideID = lastSlide.attr('id');
+		out = false;
+		$(".burger_wrapper").toggleClass("active");
+		$(this).css({'border': 'none'});
+
+
+		if(id == slide_video.attr('id'))//Slide video
+		{
+			if(id == lastSlideID)//if click to last slide
+			{
+				slide_video.addClass("active");
+				return;
+			}
+			else//if click to an other slide(about or projects)
+			{
+				if(slide_video.hasClass("middle"))//middle
+				{
+					slide_video.removeClass("middle");
+					if(lastSlideID == slide_about.attr('id'))//last was slide about
+					{
+						slide_about.removeClass("first");
+						slide_about.addClass("middle");
+					}
+					else//last was slide projects
+					{
+						slide_projects.removeClass("first");
+						slide_projects.addClass("middle");
+					}
+				}
+				else//last
+				{
+					slide_video.removeClass("last");
+					if(lastSlideID == slide_about.attr('id'))//last was slide about
+					{
+						slide_about.removeClass("first");
+						slide_about.addClass("middle");
+						slide_projects.removeClass("middle");
+						slide_projects.addClass("last");
+					}
+					else//last was slide projects
+					{
+						slide_projects.removeClass("first");
+						slide_projects.addClass("middle");
+						slide_about.removeClass("middle");
+						slide_about.addClass("last");
+					}
+				}
+			}
+			slide_about.addClass("hidden");
+			slide_projects.addClass("hidden");
+			slide_video.addClass("active");
+			slide_video.addClass("first");
+			lastSlide = slide_video;
+		}
+
+		if( id == slide_about.attr('id') )//Slide about
+		{
+			if(id == lastSlideID)//if click to last slide
+			{
+				slide_about.addClass("active");
+				return;
+			}
+			else//if click to an other slide(video or projects)
+			{
+				if(slide_about.hasClass("middle"))//middle
+				{
+					slide_about.removeClass("middle");
+					if(lastSlideID == slide_video.attr('id'))//last was slide video
+					{
+						slide_video.removeClass("first");
+						slide_video.addClass("middle");
+					}
+					else//last was slide projects
+					{
+						slide_projects.removeClass("first");
+						slide_projects.addClass("middle");
+					}
+				}
+				else//last
+				{
+					slide_about.removeClass("last");
+					if(lastSlideID == slide_video.attr('id'))//last was slide video
+					{
+						slide_video.removeClass("first");
+						slide_video.addClass("middle");
+						slide_projects.removeClass("middle");
+						slide_projects.addClass("last");
+					}
+					else//last was slide projects
+					{
+						slide_projects.removeClass("first");
+						slide_projects.addClass("middle");
+						slide_video.removeClass("middle");
+						slide_video.addClass("last");
+					}
+				}
+			}
+			slide_video.addClass("hidden");
+			slide_projects.addClass("hidden");
+			slide_about.addClass("active");
+			slide_about.addClass("first");
+			lastSlide = slide_about;
+		}
+
+		if( id == slide_projects.attr('id') )//Slide projects
+		{
+			if(id == lastSlideID)//if click to last slide
+			{
+				slide_projects.addClass("active");
+				return;
+			}
+			else//if click to an other slide(video or about)
+			{
+				if(slide_projects.hasClass("middle"))//middle
+				{
+					slide_projects.removeClass("middle");
+					if(lastSlideID == slide_video.attr('id'))//last was slide video
+					{
+						slide_video.removeClass("first");
+						slide_video.addClass("middle");
+					}
+					else//last was slide about
+					{
+						slide_about.removeClass("first");
+						slide_about.addClass("middle");
+					}
+				}
+				else//last
+				{
+					slide_projects.removeClass("last");
+					if(lastSlideID == slide_video.attr('id'))//last was slide video
+					{
+						slide_video.removeClass("first");
+						slide_video.addClass("middle");
+						slide_about.removeClass("middle");
+						slide_about.addClass("last");
+					}
+					else//last was slide about
+					{
+						slide_about.removeClass("first");
+						slide_about.addClass("middle");
+						slide_video.removeClass("middle");
+						slide_video.addClass("last");
+					}
+				}
+			}
+			slide_video.addClass("hidden");
+			slide_about.addClass("hidden");
+			slide_projects.addClass("active");
+			slide_projects.addClass("first");
+			lastSlide = slide_projects;
+		}
+
+	});
+
+});
+
+//burgDelay();
+setInterval(RandText1, 20);
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+var count_changes = 0;
+var istrue = [];
+var delay_for_main_text = 0;
+var returning = false;
+
+function RandText1()
+{
+	var main_text = "";
+	if(count_changes < 35)
+	{
+		var text_for_name = $(".main_name").text();
+		if(text_for_name == null)
+		{
+			text_for_name = $(".main_name").text();
+		}
+		var pos = Math.floor(Math.random() * text_for_name.length);
+		text_for_name = text_for_name.replace(text_for_name.charAt(pos), chars.charAt(Math.floor(Math.random() * chars.length)));
+		$(".main_name").text(text_for_name);
+ 		count_changes += 1;
+	}
+	else
+	{
+		var Namee = "Maxim Liubushkin";
+		main_txt = $(".main_name").text();
+		if(main_txt != Namee)
+		{
+			if(istrue == null)
+			{
+				for(var i = 0; i < main_txt.length; i++)
+				{
+					istrue.push("0");
+				}
+			}
+			else
+			{
+				if(returning == false)
+				{
+					for(var i = 0; i < main_txt.length; i++)
+					{
+						istrue[i] = "0";
+					}
+					returning = true;
+				}
+			}
+		}
+		for(var i = 0; i < main_txt.length; i++)
+		{
+				if(((main_txt.charAt(i) == Namee.charAt(i)) && (istrue[i] == "0")))
+				{
+					istrue[i] = "1"
+					continue;
+				}
+				else
+					if(istrue[i] == "1")
+						continue;
+				if(((main_txt.charAt(i) != Namee.charAt(i)) && (istrue[i] == "0")))
+				{
+					main_txt = main_txt.replace(main_txt.charAt(i), Namee.charAt(i));
+					istrue[i] = "1";
+					break;
+				}
+		}
+		var count_nulls = 0;
+		for(var i = 0; i<main_txt.length; i++)
+		{
+			if(istrue[i] == "0")
+				count_nulls++;
+		}
+		if(count_nulls == 0)
+		{
+			main_txt = Namee;
+			text_for_name = main_txt;
+		}
+		if(returning == true)
+			$(".main_name").text(main_txt);
+		if(main_txt == Namee)
+		{
+			delay_for_main_text++;
+			if(delay_for_main_text == 75)//1.5sec (20*75 = 1500mls = 1.5sec)
+			{
+				delay_for_main_text = 0;
+				count_changes = 0;
+			}
+			returning = false;
+		}
+	}
+}
+
+function burgDelay()
+{
+	return;
+	/*var burger = $(".burger_wrapper");
+	var delay = setInterval(function(){
+	if( (burger.hasClass("active") && out == false) || ((!burger.hasClass("active")) && out == true))
+		burger.toggleClass("active");
+	}, 200);*/
+}
+
+setInterval(TimeGo, 1000);
+function TimeGo()
+{
+	var time = new Date();
+	var h = time.getHours().toString();
+	var m = time.getMinutes().toString();
+	var s = time.getSeconds().toString();
+
+	if(h.length < 2)
+		h = '0' + h;
+	if(m.length < 2)
+		m = '0' + m;
+	if(s.length < 2)
+		s = '0' + s;
+
+	var clockResult = h + ':' + m + ':' + s;
+
+	$("#time").text(clockResult);
+}
